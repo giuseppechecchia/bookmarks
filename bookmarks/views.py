4fc1bc4f1daf21a9cb0a9c735eaeca3119580ad1
Giuseppe Checchia (production) <giuseppechecchia@gmail.com>
2022-11-10 10:14:48 +0000

    login required decorator

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -6,12 +6,15 @@ from django.urls import reverse
 from bookmarks.forms import BookmarkModelForm
 from bookmarks.models import Bookmark
 
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
+@method_decorator(login_required, name='dispatch')
 class BookmarkList(generic.ListView):
     template_name = 'bookmarks/bookmark_list.html'
     queryset = Bookmark.objects.all()
 
-
+@method_decorator(login_required, name='dispatch')
 class BookmarkListByTag(generic.ListView):
     template_name = 'bookmarks/bookmark_list.html'
 
@@ -20,7 +23,7 @@ class BookmarkListByTag(generic.ListView):
         queryset = Bookmark.objects.filter(tags__name__in=[tag])
         return queryset
 
-
+@method_decorator(login_required, name='dispatch')
 def create_bookmark(request):
     form = BookmarkModelForm(request.POST or None)
     if form.is_valid():
@@ -31,7 +34,7 @@ def create_bookmark(request):
     }
     return render(request, 'bookmarks/create_bookmark.html', context)
 
-
+@method_decorator(login_required, name='dispatch')
 def edit_bookmark(request, slug):
     bookmark = get_object_or_404(Bookmark, slug=slug)
     form = BookmarkModelForm(request.POST or None, instance=bookmark)
@@ -43,7 +46,7 @@ def edit_bookmark(request, slug):
     }
     return render(request, 'bookmarks/create_bookmark.html', context)
 
-
+@method_decorator(login_required, name='dispatch')
 def delete_bookmark(request, slug):
     bookmark = get_object_or_404(Bookmark, slug=slug)
     bookmark.delete()
--- delicious_django/settings.py
+++ delicious_django/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -23,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+handler404 = 'bookmarks.views.404'
